1ad80e658334de60dccd622367319f2e50b5abe1
FelienZ <rajabfadh@gmail.com>
2025-12-15 21:38:15 +0700

    remove negative test case for pass Ci workflows

--- src/ci-hello-world.test.js
+++
@@ -1,6 +0,0 @@
-describe('Hello world message', () => {
-    it('message should equal to hello world', () => {
-        const message = 'halo dunia';
-        expect(message).toEqual('hello world');
-    });
-});
