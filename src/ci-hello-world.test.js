470c5b00688b20b77bba9dd112346c9e8c11c2db
FelienZ <rajabfadh@gmail.com>
2025-12-15 21:27:32 +0700

    Add CI Test case (negative)

---
+++ src/ci-hello-world.test.js
@@ -0,0 +1,6 @@
+describe('Hello world message', () => {
+    it('message should equal to hello world', () => {
+        const message = 'halo dunia';
+        expect(message).toEqual('hello world');
+    });
+});
